732ec3d944998640ffd5c5c5aae780f70efc5fca
Travis Avery <travisavery82@gmail.com>
2019-07-07 09:59:05 -0500

    Initial commit of FrozenDict and FrozenList

---
+++ __init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+
+from .frozendict import FrozenDict as ReadOnlyDict
+from .frozenlist import FrozenList as ReadOnlyList
---
+++ frozenlist.py
@@ -0,0 +1,27 @@
+from collections import Collection
+
+class FrozenList(Collection):
+    '''Don't forget the docstrings'''
+
+    def __init__(self, *args, **kwargs):
+        self._l = list(*args, **kwargs)
+        self._index = -1
+
+    def __contains__(self, key):
+        return self._l.__contains__(key)
+
+    def __getitem__(self, key):
+        return self._l[key]
+
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return len(self._l)
+
+    def __next__(self):
+        self._index += 1
+        if self._index >= len(self):
+            self._index = -1
+            raise StopIteration
+        return self._l[self._index]
