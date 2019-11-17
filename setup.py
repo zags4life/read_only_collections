16a31e2f2d93746699e65740504b75efb824ff26
Travis Avery <travisavery82@gmail.com>
2019-11-17 10:55:22 -0600

    making pip installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='read_only_collections',
+    version='1.0.0',
+    author='Travis Avery',
+    python_requires='>=3.7',
+    packages=find_packages()
+)
